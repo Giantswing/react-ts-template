a6e319c118b02482f3c970870a75f4a7078287d6
Alejandro <alejandropino88@gmail.com>
2025-08-08 09:01:42 +0200

    Add @iconify/react dependency and integrate icon in StoreCounter component

--- src/pages/StoreCounter.tsx
+++ src/pages/StoreCounter.tsx
@@ -1,4 +1,5 @@
 import useStore from "@/store";
+import { Icon } from "@iconify/react";
 
 const StoreCounter = () => {
   const { count, increment, decrement, reset } = useStore();
@@ -6,6 +7,8 @@ const StoreCounter = () => {
   return (
     <div className="flex flex-col gap-4 items-center justify-center my-4">
       <h1 className="text-2xl font-bold">{count}</h1>
+      <Icon icon="mdi:home" />
+
       <div className="flex gap-4">
         <button onClick={increment} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
           +
